68bebc7a4d28eddcee42973a6ed57a8c4372f49d
louayloulouzorai <louay.zorai@esprit.tn>
2025-07-10 02:24:26 +0100

    test du webhook

--- hello_devops.py
+++ hello_devops.py
@@ -1,3 +1,4 @@
 print("Hello from DevOps !")
 print("Hello Jenkins, triggered by push!")
 print("Hello Jenkins, intervention de NGROK pour publier le lien local sur internet !")
+print("Hello Jenkins, 2eme test du build automatique via le webhook !")
