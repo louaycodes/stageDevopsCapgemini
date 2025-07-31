10559e64421daa0470457e9a4e3fb4495efd32fc
louayloulouzorai <louay.zorai@esprit.tn>
2025-07-31 10:50:45 +0100

    changement dans le script de résumé des logs pour rendre les caracteres tel que ç et é lisible

--- resume_logs.py
+++ resume_logs.py
@@ -48,7 +48,7 @@ print(summary)
 print("\n======================================\n")
 
 # Sauvegarder dans un fichier
-with open("resume_logs.txt", "w", encoding="utf-8-sig", errors="replace") as out:
+with open("resume_logs.txt", "w", encoding="cp1252", errors="replace") as out:
     out.write(summary)
 
     
